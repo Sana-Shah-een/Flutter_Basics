d2a471b59a0aeff2d370942bf3901ee65213c0b8
sana-shah-een <sanashaheen047@gmail.com>
2025-03-29 12:54:17 +0500

    created text_widget

---
+++ app_3_text_widget/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Text Widget"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Text(
+        "Hello Flutter Text",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w800,
+          backgroundColor: const Color.fromARGB(255, 178, 217, 235),
+        ),
+      ),
+    );
+  }
+}
